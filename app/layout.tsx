08c560a0f1ac83f64f2b882b01895bb6dfc22f0a
danushka <danmadushzxc@gmail.com>
2026-05-22 10:50:56 +0530

    feat:added page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,19 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Toaster
+          position="top-center"
+          toastOptions={{
+            style: {
+              background: "#022c22",
+              color: "#d1fae5",
+              border: "1px solid #065f46",
+            },
+          }}
+        />
+      </body>
     </html>
   );
 }
